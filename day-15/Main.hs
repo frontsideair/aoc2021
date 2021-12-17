0155609263439978abdc93eb92ab558aecc801cf
Fatih Altinok <photonia@gmail.com>
2021-12-17 13:35:25 +0300

    Day 15 part 2

--- day-15/Main.hs
+++ day-15/Main.hs
@@ -1,9 +1,10 @@
 import Data.Foldable (foldl')
 import Data.Heap (Entry (Entry), Heap)
 import qualified Data.Heap as Heap
+import Data.List (foldl1')
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import Data.Matrix (Matrix, (!))
+import Data.Matrix (Matrix, (!), (<->), (<|>))
 import qualified Data.Matrix as Matrix
 import Text.Parsec (count, digit, endBy1, endOfLine, eof, many1)
 import Text.Parsec.Text (Parser, parseFromFile)
@@ -12,9 +13,19 @@ main :: IO ()
 main = do
   input <- parseFromFile parser "input.txt" >>= either (error . show) return
   print $ part1 input
+  print $ part2 input
 
 part1 :: Matrix Int -> Maybe Int
 part1 input = Map.lookup (Matrix.nrows input, Matrix.ncols input) $ shortestPath input
+
+part2 :: Matrix Int -> Maybe Int
+part2 = part1 . enlarge
+
+enlarge :: Matrix Int -> Matrix Int
+enlarge = go (<->) . go (<|>)
+  where
+    go f input = foldl1' f (take 5 $ iterate (fmap inc) input)
+    inc n = if n + 1 > 9 then 1 else n + 1
 
 shortestPath :: Matrix Int -> Map (Int, Int) Int
 shortestPath matrix = go matrix knownDistances visited
