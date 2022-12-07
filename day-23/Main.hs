98743fbf7ae351be2dec1952dff7763e9c4a61ce
Fatih Altinok <photonia@gmail.com>
2022-12-07 03:30:28 +0300

    Day 23 part 2

--- day-23/Main.hs
+++ day-23/Main.hs
@@ -1,29 +1,54 @@
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE TupleSections #-}
 
-import Control.Monad ((>=>))
-import Data.Foldable (Foldable (foldl'), traverse_)
+import Control.Monad (when)
+import Data.Bifunctor (second)
+import Data.Foldable (Foldable (foldl'))
 import Data.Heap (Entry (Entry), Heap)
 import qualified Data.Heap as Heap
-import Data.List (minimumBy, sortOn)
+import Data.List (elemIndex, foldl1', minimumBy, partition)
 import Data.Map (Map)
 import Data.Map.Strict ((!), (!?))
 import qualified Data.Map.Strict as Map
-import Data.Maybe (fromMaybe)
-import Data.Ord (Down (Down), comparing)
+import Data.Matrix (Matrix, (<->))
+import qualified Data.Matrix as Matrix
+import Data.Maybe (catMaybes, fromMaybe)
+import Data.Ord (comparing)
 import Data.Set (Set)
 import qualified Data.Set as Set
-import Text.Parsec (endBy1, eof, noneOf, oneOf, skipMany)
+import System.Environment (getArgs)
+import Text.Parsec (eof, many1, newline, noneOf, parse, sepEndBy)
 import Text.Parsec.String (Parser, parseFromFile)
+
+toInsert :: [String]
+toInsert =
+  lines
+    "  #D#C#B#A#\n\
+    \  #D#B#A#C#"
+
+part1 :: IO ()
+part1 = do
+  matrix <- parseFromFile parser "input.txt" >>= either (error . show) return
+  let state = createState matrix
+  let endgames = run state
+  print $ cost $ minimumBy (comparing cost) endgames
+  return ()
+
+part2 :: IO ()
+part2 = do
+  (top, bottom) <- splitAt 3 . lines <$> readFile "input.txt"
+  let input = unlines $ top ++ toInsert ++ bottom
+  let matrix = either (error . show) id $ parse parser "string" input
+  let state = createState matrix
+  let endgames = run state
+  print $ cost $ minimumBy (comparing cost) endgames
+  return ()
 
 main :: IO ()
 main = do
-  [a, b, c, d, a', b', c', d'] <- parseFromFile parser "input.txt" >>= either (error . show) return
-  let initialState = createState [a, b, c, d, a', b', c', d']
-  print initialState
-  -- print $ head $ sortOn cost $ filter isSuccessState $ [initialState] >>= (step >=> step >=> step >=> step >=> step >=> step >=> step >=> step >=> step)
-  let endgames = run initialState
-  print $ minimumBy (comparing cost) endgames
-  return ()
+  arg <- head . (++ ["all"]) <$> getArgs
+  when (arg == "all" || arg == "part1") part1
+  when (arg == "all" || arg == "part2") part2
 
 run :: State -> [State]
 run state =
@@ -41,96 +66,97 @@ energy _ = error "no such amphipod"
 type Coord = (Int, Int)
 
 data State = State
-  { amphipods :: Map Coord Char,
-    spaces :: Set Coord,
+  { matrix :: Matrix Char,
+    amphipods :: Map Coord Char,
     corridors :: [Coord], -- size 7
-    rooms :: Map Char [Coord], -- [outer, inner]
-    cost :: Int
+    rooms :: Map Char [Coord], -- gate:rest
+    cost :: Int,
+    paths :: Map (Coord, Coord) (Set Coord)
   }
 
-instance Show State where
-  show State {cost, amphipods, rooms, corridors} =
-    unlines
-      [ "#############",
-        "#" ++ s _1 ++ s _2 ++ "." ++ s _3 ++ "." ++ s _4 ++ "." ++ s _5 ++ "." ++ s _6 ++ s _7 ++ "#",
-        "###" ++ s a1 ++ "#" ++ s b1 ++ "#" ++ s c1 ++ "#" ++ s d1 ++ "###",
-        "  #" ++ s a2 ++ "#" ++ s b2 ++ "#" ++ s c2 ++ "#" ++ s d2 ++ "###",
-        "  #########",
-        "Cost: " ++ show cost
-      ]
-    where
-      [a1, a2] = (amphipods !?) <$> (rooms ! 'A')
-      [b1, b2] = (amphipods !?) <$> (rooms ! 'B')
-      [c1, c2] = (amphipods !?) <$> (rooms ! 'C')
-      [d1, d2] = (amphipods !?) <$> (rooms ! 'D')
-      [_1, _2, _3, _4, _5, _6, _7] = (amphipods !?) <$> corridors
-      s (Just a) = [a]
-      s Nothing = "."
-
-createState :: [Char] -> State
-createState [a, b, c, d, a', b', c', d'] =
+createState :: Matrix Char -> State
+createState matrix =
   State
-    { amphipods = amphipods,
-      spaces = Set.fromList $ roomSpaces ++ corridors ++ [(2, 4), (2, 6), (2, 8), (2, 10)],
+    { matrix = matrix,
+      amphipods = amphipods,
       corridors = corridors,
-      rooms = Map.fromList [('A', [(3, 4), (4, 4)]), ('B', [(3, 6), (4, 6)]), ('C', [(3, 8), (4, 8)]), ('D', [(3, 10), (4, 10)])],
-      cost = 0
+      rooms = rooms,
+      cost = 0,
+      paths = paths
     }
   where
-    amphipods = Map.fromList $ zip roomSpaces [a, b, c, d, a', b', c', d']
-    roomSpaces = [(3, 4), (3, 6), (3, 8), (3, 10), (4, 4), (4, 6), (4, 8), (4, 10)]
+    room (x, y) = (,y) <$> take (Matrix.nrows matrix - 3) [x ..]
+    rooms = Map.fromList $ second room <$> [('A', (3, 4)), ('B', (3, 6)), ('C', (3, 8)), ('D', (3, 10))]
+    amphipods = Map.fromList $ (\(x, y) -> ((x, y), Matrix.getElem x y matrix)) <$> concat rooms
     corridors = [(2, 2), (2, 3), (2, 5), (2, 7), (2, 9), (2, 11), (2, 12)]
-createState _ = error "wrong number of amphipods"
+    empty = corridors ++ concat rooms
+    paths = Map.fromList [((from, to), aStar from to matrix) | from <- empty, to <- empty, from /= to]
 
 isAmphipodTerminal :: State -> Coord -> Bool
-isAmphipodTerminal State {amphipods, rooms} coord =
-  let amphipod = amphipods ! coord
-      [outer, inner] = rooms ! amphipod
-      amphipodAtInnerPart = coord == inner
-      amphipodAtOuterPartAndInnerPartHasFriend = coord == outer && (amphipods !? inner) == Just amphipod
-   in amphipodAtInnerPart || amphipodAtOuterPartAndInnerPartHasFriend
+isAmphipodTerminal State {amphipods, rooms} coord = fromMaybe False $ do
+  amphipod <- amphipods !? coord
+  roomCoords <- rooms !? amphipod
+  index <- elemIndex coord roomCoords
+  let innerPart = drop index roomCoords
+  return $ all (== Just amphipod) $ (amphipods !?) <$> innerPart
 
 isSuccessState :: State -> Bool
 isSuccessState state@State {rooms, amphipods} =
   all
-    (\(amphipod, [outer, inner]) -> (amphipods !? outer) == Just amphipod && (amphipods !? inner) == Just amphipod)
+    (\(amphipod, roomCoords) -> all (== Just amphipod) $ (amphipods !?) <$> roomCoords)
     (Map.toList rooms)
 
-shortestPath :: State -> Coord -> Coord -> Map Coord Int
-shortestPath state from to = go state knownDistances visited
+manhattan :: Coord -> Coord -> Int
+manhattan (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
+
+reconstructPath :: Map Coord Coord -> Coord -> Set Coord
+reconstructPath cameFrom current =
+  case cameFrom !? current of
+    Nothing -> Set.empty
+    Just previous -> Set.insert current $ reconstructPath cameFrom previous
+
+aStar :: Coord -> Coord -> Matrix Char -> Set Coord
+aStar from to matrix = go openSet cameFrom gScore
   where
-    knownDistances = Heap.singleton $ Entry 0 from
-    visited = Map.empty
-    go state knownDistances visited = case Heap.uncons knownDistances of
-      Nothing -> visited
-      Just (Entry distV v, knownDistances') -> go state knownDistances'' visited'
+    openSet = Heap.singleton (Entry (manhattan from to) from)
+    cameFrom = Map.empty
+    gScore = Map.singleton from 0
+    go openSet cameFrom gScore = case Heap.uncons openSet of
+      Nothing -> Set.empty -- failure
+      Just (Entry _ current, openSet') ->
+        if current == to
+          then reconstructPath cameFrom current
+          else go openSet'' cameFrom' gScore'
         where
-          ns = filter (`Map.notMember` visited) (Set.toList $ neighborSpaces state v)
-          dists = (+ distV) . const 1 <$> ns
-          knownDistances'' = foldl' (updatePriority min) knownDistances' (zipWith Entry dists ns)
-          visited' = Map.insert v distV visited
+          (openSet'', cameFrom', gScore') = foldl' f (openSet', cameFrom, gScore) (neighborSpaces matrix current)
+          f (o, c, g) n =
+            let tentativeGScore = (case gScore !? current of Just n -> n; Nothing -> error "here") + 1
+             in if case gScore !? n of Nothing -> True; Just x -> tentativeGScore < x
+                  then
+                    ( Heap.insert (Entry (tentativeGScore + manhattan n to) n) o,
+                      Map.insert n current c,
+                      Map.insert n tentativeGScore g
+                    )
+                  else (o, c, g)
 
-updatePriority :: (Ord p, Eq a) => (p -> p -> p) -> Heap (Entry p a) -> Entry p a -> Heap (Entry p a)
-updatePriority f heap (Entry a b) = Heap.insert new tail
+neighborSpaces :: Matrix Char -> Coord -> Set Coord
+neighborSpaces matrix (x, y) =
+  Set.fromList $
+    catMaybes $
+      ( \(x, y) -> do
+          char <- Matrix.safeGet x y matrix
+          if isWalkable char then Just (x, y) else Nothing
+      )
+        <$> neighborCoords (x, y)
+
+neighborCoords :: Coord -> [Coord]
+neighborCoords (x, y) = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+
+isWalkable :: Char -> Bool
+isWalkable char = char `notElem` [void, wall]
   where
-    (head, tail) = Heap.partition (\(Entry _ b') -> b == b') heap
-    Entry old _ = Heap.minimum head
-    new = if Heap.null head then Entry a b else Entry (f old a) b
-
-findPath :: Coord -> Coord -> State -> Set Coord
-findPath from to state = findPath' (shortestPath state to from) Set.empty from to state
-
-findPath' :: Map Coord Int -> Set Coord -> Coord -> Coord -> State -> Set Coord
-findPath' shortestPathTree path from to state = if coordToMove == to then path' else findPath' shortestPathTree path' coordToMove to state
-  where
-    neighbors = Set.toList $ neighborSpaces state from `Set.difference` path
-    coordToMove
-      | null neighbors = error $ show (from, to, neighbors, path, shortestPathTree)
-      | otherwise = minimumBy (comparing (shortestPathTree !?)) neighbors
-    path' = Set.insert from path
-
-neighborSpaces :: State -> Coord -> Set Coord
-neighborSpaces State {spaces} (x, y) = Set.fromList [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)] `Set.intersection` spaces
+    void = ' '
+    wall = '#'
 
 isPathUnblocked :: Set Coord -> State -> Bool
 isPathUnblocked path State {amphipods} = path `Set.disjoint` Map.keysSet amphipods
@@ -141,37 +167,29 @@ isUnblocked State {amphipods} coord = coord `Map.notMember` amphipods
 ownRoom :: State -> Coord -> [Coord]
 ownRoom State {amphipods, rooms} coord = rooms ! (amphipods ! coord)
 
-roomAmphipodsThatCanMove :: State -> [Coord]
-roomAmphipodsThatCanMove state@State {amphipods, corridors} = filter (`notElem` corridors) $ filter (not . isAmphipodTerminal state) (Map.keys amphipods)
-
-corridorAmphipods :: State -> [Coord]
-corridorAmphipods State {amphipods, corridors} = filter (`elem` corridors) (Map.keys amphipods)
-
 step :: State -> [State]
-step state = if null finalMoves then corridorMoves else finalMoves
+step state@State {amphipods, corridors, paths} = if null finalMoves then corridorMoves else finalMoves
   where
+    (corridorAmphipods, roomAmphipods) = partition (`elem` corridors) (Map.keys amphipods)
+    roomAmphipodsThatCanMove = filter (not . isAmphipodTerminal state) roomAmphipods
     corridorMoves =
       [ move state (Set.size path) from to
-        | from <- roomAmphipodsThatCanMove state,
-          to <- filter (isUnblocked state) $ corridors state,
-          let path = findPath to from state,
+        | from <- roomAmphipodsThatCanMove,
+          to <- corridors,
+          let path = paths ! (from, to),
+          isUnblocked state to,
           isPathUnblocked path state
       ]
     finalMoves =
       [ nextState
-        | from <- roomAmphipodsThatCanMove state,
-          to <- filter (isUnblocked state) $ ownRoom state from,
-          let path = findPath to from state,
+        | from <- corridorAmphipods ++ roomAmphipodsThatCanMove,
+          to <- ownRoom state from,
+          let path = paths ! (from, to),
           let nextState = move state (Set.size path) from to,
-          isPathUnblocked path state && isAmphipodTerminal nextState to
+          isUnblocked state to,
+          isPathUnblocked path state,
+          isAmphipodTerminal nextState to
       ]
-        ++ [ nextState
-             | from <- corridorAmphipods state,
-               to <- filter (isUnblocked state) $ ownRoom state from,
-               let path = findPath to from state,
-               let nextState = move state (Set.size path) from to,
-               isPathUnblocked path state && isAmphipodTerminal nextState to
-           ]
 
 move :: State -> Int -> Coord -> Coord -> State
 move state@State {amphipods, cost} steps from to =
@@ -194,11 +212,10 @@ repeat if list of states is not empty
 -- amphipod is not at its final state -> can move into its own room (if empty or has friend) or corridor
 -- amphipod is at corridor -> can move into its own room (if empty or has friend)
 
-parser :: Parser [Char]
-parser = skip *> amphipod `endBy1` skip <* eof
+parser :: Parser (Matrix Char)
+parser = do
+  rows <- row `sepEndBy` newline <* eof
+  return $ foldl1' (<->) rows
 
-skip :: Parser ()
-skip = skipMany (noneOf "ABCD")
-
-amphipod :: Parser Char
-amphipod = oneOf "ABCD"
+row :: Parser (Matrix Char)
+row = Matrix.fromList 1 13 . (++ repeat ' ') <$> many1 (noneOf "\n")
